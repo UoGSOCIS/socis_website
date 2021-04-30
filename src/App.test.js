aac286472d3cd0da76cddd079e0a6b935f34821b
SoorajModi <soomodi@gmail.com>
2021-04-30 15:06:36 -0400

    Fix tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
+import React from "react";
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Hello World/i);
+  const linkElement = screen.getByText(/SOCIS/i);
   expect(linkElement).toBeInTheDocument();
 });
