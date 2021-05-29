9b2cb1f509bb76f7e1870c94cd90dace68c10197
danindugit <dmarasin@uoguelph.ca>
2021-05-29 17:45:45 -0400

    Recolor and resize

--- src/components/buttons/LearnMoreBtn.jsx
+++ src/components/buttons/LearnMoreBtn.jsx
@@ -5,7 +5,7 @@ import PropTypes from 'prop-types';
 function LearnMoreBtn({ href }) {
   return (
     <div className="LearnMoreBtn">
-      <Button variant="contained" color="primary" href={href}>Learn More</Button>
+      <Button variant="contained" color="primary" style={styles.learnMoreStyle} href={href}>Learn More</Button>
     </div>
   );
 }
@@ -16,6 +16,14 @@ LearnMoreBtn.propTypes = {
 
 LearnMoreBtn.defaultProps = {
   href: '/',
+};
+
+const styles = {
+  learnMoreStyle: {
+    backgroundColor: 'rgb(102, 0, 0)',
+    padding: '10px 20px 10px 20px',
+    fontSize: '14px',
+  },
 };
 
 export default LearnMoreBtn;
