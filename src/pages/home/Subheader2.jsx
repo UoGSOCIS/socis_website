132076812d256680ceafe37be57f6f29898d9a95
danindugit <dmarasin@uoguelph.ca>
2021-05-29 16:58:56 -0400

    Make content responsive

--- src/pages/home/Subheader2.jsx
+++ src/pages/home/Subheader2.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
-import { LearnMoreBtn } from '../../components';
+import Grid from '@material-ui/core/Grid';
+import { LearnMoreBtn, useViewport } from '../../components';
 
 function Subheader2() {
+  const { width } = useViewport();
+  const breakpoint = 959;
+
   return (
-    <div className="subheader2" style={styles.subheader}>
+    <div
+      className="subheader2"
+      style={
+      width > breakpoint
+        ? styles.subheader
+        : styles.subheaderMobile
+      }
+    >
       <h1>What do we do?</h1>
-      <p>
-        Sint sunt anim magna non labore incididunt mollit in fugiat dolore et.
-        Commodo anim minim occaecat adipisicing mollit.
-        Nisi amet nisi duis et aliqua aute non laborum velit.
-        Dolore esse Lorem Lorem reprehenderit consequat cillum aute eu cillum.
-        Ullamco excepteur nostrud dolore ad mollit enim occaecat et anim tempor
-        sint pariatur dolore.
-      </p>
+      <Grid container spacing={5}>
+        <Grid item md={3}>
+          <img src="placeholder.png" alt="placeholder" style={{ maxWidth: '100%' }} />
+        </Grid>
+        <Grid item md={9}>
+          <p>
+            Sint sunt anim magna non labore incididunt mollit in fugiat dolore et.
+            Commodo anim minim occaecat adipisicing mollit.
+            Nisi amet nisi duis et aliqua aute non laborum velit.
+            Dolore esse Lorem Lorem reprehenderit consequat cillum aute eu cillum.
+            Ullamco excepteur nostrud dolore ad mollit enim occaecat et anim tempor
+            sint pariatur dolore.
+          </p>
+        </Grid>
+      </Grid>
       <LearnMoreBtn href="/clubs" />
     </div>
   );
@@ -23,6 +41,11 @@ const styles = {
     paddingTop: '5vh',
     paddingBottom: '5vh',
     textAlign: 'right',
+  },
+  subheaderMobile: {
+    paddingTop: '5vh',
+    paddingBottom: '5vh',
+    textAlign: 'center',
   },
 };
 
