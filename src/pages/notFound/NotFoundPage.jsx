78ab32177eeaf4b677d9f953e248a0789e7321cc
SoorajModi <soomodi@gmail.com>
2021-05-29 20:20:26 -0400

    Add not found page contents

--- src/pages/notFound/NotFoundPage.jsx
+++ src/pages/notFound/NotFoundPage.jsx
@@ -1,11 +1,35 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import { NavBar } from '../../components';
 
 function NotFoundPage() {
   return (
-    <div className="notFound">
-      <h1>404 Not Found</h1>
-    </div>
+    <>
+      <NavBar />
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={styles.grid}
+      >
+        <h1 style={styles.header}>404 not found</h1>
+        <p>Looks like you took a wrong turn :(</p>
+        <Button href="/">Return Home</Button>
+      </Grid>
+    </>
   );
 }
+
+const styles = {
+  grid: {
+    height: '80vh',
+  },
+  header: {
+    marginBottom: '0',
+  },
+};
 
 export default NotFoundPage;
