cf9d24ef4bdceac107712e023707ed4de81ec7c6
danindugit <dmarasin@uoguelph.ca>
2021-06-12 02:06:51 -0400

    Removed unnecessary elements

--- src/components/buttons/LearnMoreBtn.jsx
+++ src/components/buttons/LearnMoreBtn.jsx
@@ -4,9 +4,7 @@ import PropTypes from 'prop-types';
 
 function LearnMoreBtn({ href }) {
   return (
-    <div className="LearnMoreBtn">
-      <Button variant="contained" color="primary" style={styles.learnMoreStyle} href={href}>Learn More</Button>
-    </div>
+    <Button className="LearnMoreBtn" variant="contained" color="primary" style={style} href={href}>Learn More</Button>
   );
 }
 
@@ -18,12 +16,10 @@ LearnMoreBtn.defaultProps = {
   href: '/',
 };
 
-const styles = {
-  learnMoreStyle: {
-    backgroundColor: 'rgb(102, 0, 0)',
-    padding: '10px 20px 10px 20px',
-    fontSize: '14px',
-  },
+const style = {
+  backgroundColor: 'rgb(102, 0, 0)',
+  padding: '10px 20px 10px 20px',
+  fontSize: '14px',
 };
 
 export default LearnMoreBtn;
--- src/pages/home/header.jsx
+++ src/pages/home/header.jsx
@@ -1,29 +1,26 @@
 import React from 'react';
-import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 import HeaderMessage from './headerMessage';
 
 function Header() {
   return (
     <div className="homeHeading" style={styles.banner}>
-      <Box
-        display="flex"
-        width="100%"
-        height="85%"
+      <Grid
+        container
+        direction="row"
+        justify="center"
+        alignItems="center"
+        style={{ height: '100%', width: '100%' }}
       >
-        <Box m="auto">
-          <Grid container>
-            <Grid item md={4} style={{ marginLeft: 'auto', marginRight: 'auto' }}>
-              <div style={{ textAlign: 'center' }}>
-                <img src="socisWhite.png" alt="Socis logo" className="animate__animated animate__fadeIn" />
-              </div>
-            </Grid>
-            <Grid item md={8}>
-              <HeaderMessage />
-            </Grid>
-          </Grid>
-        </Box>
-      </Box>
+        <Grid item md={4} sm={12}>
+          <div style={{ textAlign: 'center', width: '100%' }}>
+            <img src="socisWhite.png" alt="Socis logo" className="animate__animated animate__fadeIn" style={{ height: '50vh' }} />
+          </div>
+        </Grid>
+        <Grid item md={8} sm={12}>
+          <HeaderMessage />
+        </Grid>
+      </Grid>
     </div>
   );
 }
