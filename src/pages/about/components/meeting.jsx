182886b7ca7d424b61de1beefe91eb797b6f795b
SoorajModi <soomodi@gmail.com>
2021-06-18 01:02:00 -0400

    Add exec info components

--- src/pages/about/components/meeting.jsx
+++ src/pages/about/components/meeting.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 function Meeting() {
   return (
     <>
-      <h2>Meeting Information</h2>
+      <h1>Meeting Information</h1>
       <p>
-        We meet every Thursday at 5:30pm in Reynolds 0001. Join #socis on gryph.slack.com or follow our instagram
-        for
-        the latest meeting information.
+        We meet every Thursday at 5:30pm in Reynolds 0001. Join #socis on gryph.slack.com or follow our
+        {' '}
+        <a href="https://www.instagram.com/socis.uog" rel="noopener noreferrer" target="_blank">Instagram</a>
+        {' '}
+        for the latest meeting information.
       </p>
     </>
   );
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import Container from '@material-ui/core/Container';
+import styled from 'styled-components';
 import { NavBar } from '../../components';
 import Header from './components/header';
 import Meeting from './components/meeting';
+import Exec from './components/exec';
+
+const Wrapper = styled.div`
+  padding-top: 2vh;
+  padding-bottom: 10vh;
+  padding-left: 5vw;
+  padding-right: 5vw;
+`;
 
 function AboutPage() {
   return (
     <div className="about">
       <NavBar />
-      <Container>
+      <Wrapper>
         <Header />
         <Meeting />
-      </Container>
+        <Exec />
+      </Wrapper>
     </div>
   );
 }
