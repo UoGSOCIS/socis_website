47dc4b9544ea48defe0b444d5a03b765edbe9fd7
SoorajModi <soomodi@gmail.com>
2021-06-18 19:34:02 -0400

    Extract Wrapper into shared component ContentWrapper

---
+++ src/components/ContentWrapper/index.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const ContentWrapper = styled.div`
+  padding-top: 2vh;
+  padding-bottom: 10vh;
+  padding-left: 5vw;
+  padding-right: 5vw;
+`;
+
+export default ContentWrapper;
--- src/pages/about/index.jsx
+++ src/pages/about/index.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
-import styled from 'styled-components';
-import { NavBar } from '../../components';
+import { NavBar, ContentWrapper } from '../../components';
 import Header from './components/header';
 import Meeting from './components/meeting';
 import Exec from './components/exec';
 
-const Wrapper = styled.div`
-  padding-top: 2vh;
-  padding-bottom: 10vh;
-  padding-left: 5vw;
-  padding-right: 5vw;
-`;
-
 function AboutPage() {
   return (
-    <div className="about">
+    <>
       <NavBar />
-      <Wrapper>
+      <ContentWrapper>
         <Header />
         <Meeting />
         <Exec />
-      </Wrapper>
-    </div>
+      </ContentWrapper>
+    </>
   );
 }
 
