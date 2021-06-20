b90cc13b6586595d92347ce0c95e473e2c9555a1
BenPearo <benpearo@gmail.com>
2021-06-20 11:46:06 -0400

    replace placeholder with socis logo

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import {
-  Toolbar, Typography, Button, AppBar, Hidden,
+  Toolbar, Button, AppBar, Hidden,
 } from '@material-ui/core';
-// import MenuIcon from '@material-ui/icons/Menu';
 import styled from 'styled-components';
 import NavDrawer from './navdrawer';
+import logo from '../../assets/socis_logo.png';
 
 const navLinks = [
   { title: 'Clubs', path: '/clubs' },
@@ -15,35 +15,67 @@ const navLinks = [
 
 function NavBar() {
   return (
-    <AppBar position="static" style={{ backgroundColor: '#660000' }}>
-      <Toolbar>
-        <Hidden smDown>
-          <Typography variant="h6">
-            <a href="/" style={styles.homeLink}>SOCIS</a>
-          </Typography>
+    <StyledAppBar position="static" style={styles.appBar}>
+      <StyledToolbar>
+        <a href="/">
+          <Image src={logo} alt="SOCIS Logo" />
+        </a>
+        <WebView smDown>
           <Links>
             {navLinks.map(({ title, path }) => (
-              <Button color="inherit" href={path}>{title}</Button>
+              <Button color="inherit" href={path} style={styles.pageLink}>{title}</Button>
             ))}
           </Links>
-        </Hidden>
-        <Hidden mdUp>
+        </WebView>
+        <MobileView mdUp>
           <NavDrawer navLinks={navLinks} />
-        </Hidden>
-      </Toolbar>
-    </AppBar>
+        </MobileView>
+      </StyledToolbar>
+    </StyledAppBar>
   );
 }
 
-const Links = styled.div`
-  margin-left: 'auto';
-  margin-right: -12;
+const StyledAppBar = styled(AppBar)`
+  width: 100%;
 `;
+
+const StyledToolbar = styled(Toolbar)`
+  width: 100%;
+  flex-direction: row;
+  justify-content: space-between
+`;
+
+const Links = styled.div`
+`;
+
+const Image = styled.img`
+  margin-right: 0.5rem;
+  margin-top: 0.2rem;
+  margin-bottom: 0.2rem;
+  height: 4rem;
+`;
+
+const WebView = styled(Hidden)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const MobileView = styled(Hidden)``;
 
 const styles = {
   homeLink: {
     color: 'white',
     textDecoration: 'none',
+  },
+  appBar: {
+    backgroundColor: '#660000',
+    flexDirection: 'row',
+    width: '100%',
+  },
+  pageLink: {
+    height: '100%',
   },
 };
 
