dd3bb064269bf02dcec9325bbca3d084a5d4ea30
SoorajModi <soomodi@gmail.com>
2021-06-23 22:32:41 -0400

    Add social media information

--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -6,11 +6,12 @@ function ContactPage() {
   return (
     <div className="contact">
       <NavBar />
-      <h1 style={styles.header}>Contact Page</h1>
+      <h1 style={styles.header}>Contact SOCIS</h1>
       <p style={styles.description}>
-        {/* eslint-disable-next-line max-len */}
-        For immediate inquiries please reach out to us at exec@socis.ca. Also follow us on social media! For students
-        we are located at Reynold 0010, pop by to say hi!
+        We would love to hear your comments, questions, suggestions, or feedback! Please email us at
+        {' '}
+        <a href="mailto:exec@socis.ca">exec@socis.ca</a>
+        .
       </p>
       <SocialMedia />
     </div>
@@ -20,9 +21,12 @@ function ContactPage() {
 const styles = {
   header: {
     textAlign: 'center',
+    marginTop: '15vh',
+    fontSize: '54px',
   },
   description: {
     textAlign: 'center',
+    fontSize: '22px',
   },
 };
 
