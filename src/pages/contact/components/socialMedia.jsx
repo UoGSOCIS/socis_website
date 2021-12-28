3d2660a153c2351cb1c0c279aa71b68ae8106db7
SoorajModi <soomodi@gmail.com>
2021-12-28 00:42:45 -0500

    Refactor contact page components

--- src/pages/contact/components/socialMedia.jsx
+++ src/pages/contact/components/socialMedia.jsx
@@ -4,12 +4,13 @@ import {
   faDiscord, faFacebook, faInstagram, faLinkedin, faSlack, faTwitter,
 } from '@fortawesome/free-brands-svg-icons';
 import Container from '@material-ui/core/Container';
+import styled from 'styled-components';
 import { FontAwesomeLink } from '../../../components/icons';
 
 function SocialMedia() {
   return (
     <Container>
-      <h2 style={styles.header}>Follow us on social media</h2>
+      <Title>Follow us on social media</Title>
       <Grid
         container
         direction="row"
@@ -17,38 +18,37 @@ function SocialMedia() {
         alignItems="center"
         spacing={2}
       >
-        <Grid item xs={12} sm={6} md={4} lg={2} style={styles.grid}>
+        <IconGrid item xs={12} sm={6} md={4} lg={2}>
           <FontAwesomeLink href="https://www.facebook.com/groups/UoGSOCIS" icon={faFacebook} />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2} style={styles.grid}>
+        </IconGrid>
+        <IconGrid item xs={12} sm={6} md={4} lg={2}>
           <FontAwesomeLink href="https://twitter.com/uogsocis" icon={faTwitter} />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2} style={styles.grid}>
+        </IconGrid>
+        <IconGrid item xs={12} sm={6} md={4} lg={2}>
           <FontAwesomeLink href="https://www.instagram.com/socis.uog" icon={faInstagram} />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2} style={styles.grid}>
+        </IconGrid>
+        <IconGrid item xs={12} sm={6} md={4} lg={2}>
           <FontAwesomeLink href="https://www.linkedin.com/company/uog-socis/" icon={faLinkedin} />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2} style={styles.grid}>
+        </IconGrid>
+        <IconGrid item xs={12} sm={6} md={4} lg={2}>
           <FontAwesomeLink href="https://gryph.slack.com" icon={faSlack} />
-        </Grid>
-        <Grid item xs={12} sm={6} md={4} lg={2} style={styles.grid}>
+        </IconGrid>
+        <IconGrid item xs={12} sm={6} md={4} lg={2}>
           <FontAwesomeLink href="http://discord.gg/PZ45Jn9MN7" icon={faDiscord} />
-        </Grid>
+        </IconGrid>
       </Grid>
     </Container>
   );
 }
 
-const styles = {
-  header: {
-    textAlign: 'center',
-    margin: '5vh',
-    fontSize: '36px',
-  },
-  grid: {
-    textAlign: 'center',
-  },
-};
+const Title = styled.p`
+  text-align: center;
+  margin: 5vh;
+  font-size: 36px;
+`;
+
+const IconGrid = styled(Grid)`
+  text-align: center;
+`;
 
 export default SocialMedia;
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
+import styled from 'styled-components';
 import SocialMedia from './components/socialMedia';
+import { ContentWrapper } from '../../components';
 
 function ContactPage() {
   return (
-    <div className="contact-main" style={styles.main}>
-      <h1 style={styles.header}>Contact SOCIS</h1>
-      <p style={styles.description}>
+    <ContentWrapper>
+      <Title>Contact SOCIS</Title>
+      <Subtext>
         We would love to hear your comments, questions, suggestions, or feedback! Please email us at
         {' '}
-        <a href="mailto:exec@socis.ca" style={styles.link}>exec@socis.ca</a>
+        <SubtextLink href="mailto:exec@socis.ca">exec@socis.ca</SubtextLink>
         .
-      </p>
+      </Subtext>
       <SocialMedia />
-    </div>
+    </ContentWrapper>
   );
 }
 
-const styles = {
-  main: {
-    paddingBottom: '15vh',
-  },
-  header: {
-    textAlign: 'center',
-    marginTop: '10vh',
-    fontSize: '54px',
-  },
-  description: {
-    textAlign: 'center',
-    fontSize: '22px',
-  },
-  link: {
-    color: 'black',
-    textDecoration: 'none',
-  },
-};
+const Title = styled.p`
+  text-align: center;
+  margin-top: 10vh;
+  font-size: 54px;
+`;
+
+const Subtext = styled.p`
+  font-size: 22px;
+  text-align: center;
+`;
+
+const SubtextLink = styled.a`
+  color: black;
+  text-decoration: none;
+  &:hover {
+    text-decoration: underline;
+  }
+`;
 
 export default ContactPage;
