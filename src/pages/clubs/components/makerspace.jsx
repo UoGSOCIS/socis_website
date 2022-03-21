fcd670fa2dd5a5a71737d4d7504bd1aca0f6ef61
SoorajModi <soomodi@gmail.com>
2022-03-21 18:48:54 -0400

    Enforce more consistent text style across app

--- src/pages/clubs/components/makerspace.jsx
+++ src/pages/clubs/components/makerspace.jsx
@@ -3,17 +3,14 @@ import styled from 'styled-components';
 
 function Makerspace() {
   return (
-    <>
+    <div>
       <Name>Makerspace</Name>
       <Bio>
         The Makerspace is a place for all computing and engineering students to bring their innovative project ideas to
         life. We offer you the facility to rent out hardware inventory items and access various in-house services at
         zero cost. Come join us and work with your peers in a collaborative environment to develop amazing projects!
       </Bio>
-      {/* <div style={{textAlign: 'center'}}> */}
-      {/*  <Link href="https://www.instagram.com/gcc_uog" target="_blank" rel="noreferrer">Instagram</Link> */}
-      {/* </div> */}
-    </>
+    </div>
   );
 }
 
@@ -21,16 +18,12 @@ const Name = styled.p`
     font-size: 28px;
     text-align: center;
     margin-top: 10px;
+    font-weight: 500;
 `;
 
 const Bio = styled.p`
-    font-size: 18px;
-    line-height: 1.4;
+    font-size: 20px;
+    line-height: 1.625;
 `;
-
-// const Link = styled.a`
-//     text-decoration: none;
-//     color: #000;
-// `;
 
 export default Makerspace;
