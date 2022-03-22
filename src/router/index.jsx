fd5a4f54bb36a6ffd10151dbf479101c3f107083
Marshall Asch <maasch@rogers.com>
2022-03-21 21:22:15 -0400

    removed the switch statement

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import {
   AboutPage,
   ClubPage, ContactPage,
@@ -9,14 +9,14 @@ import {
 function Router() {
   return (
     <BrowserRouter>
-      <Switch>
+      <Routes>
         <Route exact path="/" component={HomePage} />
         <Route exact path="/clubs" component={ClubPage} />
         <Route exact path="/policy" component={PolicyPage} />
         <Route exact path="/about" component={AboutPage} />
         <Route exact path="/contact" component={ContactPage} />
         <Route component={NotFoundPage} />
-      </Switch>
+      </Routes>
     </BrowserRouter>
   );
 }
