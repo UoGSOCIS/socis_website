9939cf918c100283bf55e554fc0a1c2f1fbcda95
Jvarkul <jessevarkul@gmail.com>
2023-08-19 15:25:34 -0400

    updated adam image link

--- src/pages/about/components/execList.js
+++ src/pages/about/components/execList.js
@@ -22,7 +22,7 @@ export default {
     {
       position: 'Vice President Finance',
       name: 'Adam Cote',
-      image: 'https://raw.githubusercontent.com/UoGSOCIS/assets/main/exec/2023-2024/adam-cote.jpg',
+      image: 'https://raw.githubusercontent.com/UoGSOCIS/assets/main/exec/2023-2024/adam-cote.JPG',
     },
     {
       position: 'Vice President Communication',
